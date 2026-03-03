c52133d9c39a38809bb9f4944d162fa8aad03a50
cc.zhuang <cc.zhuang@weget.jp>
2026-03-04 02:14:00 +0900

    docker compose 添加 frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const proxyTarget = process.env.VITE_PROXY_TARGET || 'http://localhost:3001'
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: proxyTarget,
         changeOrigin: true,
         secure: false,
       }
